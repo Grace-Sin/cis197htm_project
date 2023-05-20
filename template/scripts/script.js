7cf23baf02085ededc9252fab5b5ffc92ca908a4
Grace Sinor <g.sinor1@saints.mhcc.edu>
2023-05-19 17:07:18 -0700

    Wireframes & Template

---
+++ template/scripts/script.js
@@ -0,0 +1,9 @@
+/*
+    Student Name: Grace Sinor
+    File Name: script.js
+    Date: 05/19/2023
+*/
+//jQuery for hero image to consume the header window space
+$(document).ready(function() {
+    $('.hero').height($(window).height());
+});
